0d778c782bee85354df383aecd3ce5fa1e7e8067
Anton <Skilrex@live.ru>
2013-07-25 11:36:16 +0400

    "Hello"

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,5 @@
-/**
- * Created with IntelliJ IDEA.
- * User: skilrex
- * Date: 25.07.13
- * Time: 11:31
- * To change this template use File | Settings | File Templates.
- */
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
 }
