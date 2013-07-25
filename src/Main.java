0c47c477e0eb063b93b62bde4d7201ae30bb7181
Anton <Skilrex@live.ru>
2013-07-25 11:33:04 +0400

    clear project

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: skilrex
+ * Date: 25.07.13
+ * Time: 11:31
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+}
