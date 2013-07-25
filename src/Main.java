5b589f1dbd6e61c028590c92cd97a28b192efadc
Anton <Skilrex@live.ru>
2013-07-25 11:52:55 +0400

    -

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello. It's a game tick-tack-toe.");
+        System.out.println("Please type what symbol you want: 1 - X, 2 - O");
+        Player playerO = new Player("O");
+        Player playerX = new Player("X");
+        switch (scanner.nextInt()){
+            case 0:
+
+        }
+
     }
 }
---
+++ src/Player.java
@@ -0,0 +1,14 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: skilrex
+ * Date: 25.07.13
+ * Time: 11:41
+ * To change this template use File | Settings | File Templates.
+ */
+public class Player {
+    private String symbol;
+
+    public Player(String s){
+        symbol = s;
+    }
+}
